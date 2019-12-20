ac7fe8c3c2199d8ce17623b913c5851ca842caed
webwerks <raj.kamadiya@neosofttech.com>
2019-12-20 20:22:34 +0530

    save changes

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,ElementRef, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from "@angular/router";
 import { PasswordStrengthValidator } from '../../helpers/password-strength.validator';
@@ -6,7 +6,7 @@ import { MobileValidator } from '../../helpers/mobile.validator';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from "../../_services/auth.service";
 import { User } from '../../models/user';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-profile',
@@ -27,7 +27,8 @@ export class UserProfileComponent implements OnInit {
     private formBuilder: FormBuilder,
     private AuthService: AuthService,
     private toastr: ToastrService,
-    private activatedRoute :ActivatedRoute
+    private activatedRoute :ActivatedRoute,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
@@ -43,8 +44,13 @@ userData = this.activatedRoute.snapshot.data['event'].data.userData;
 
   fileProgress(fileInput: any) {
     this.fileData = <File>fileInput.target.files[0];
+    console.log(this.fileData)
     this.preview();
 }
+
+  public delete(){
+    this.url = '';
+  }
 
 preview() {
   // Show preview 
@@ -56,7 +62,7 @@ preview() {
   var reader = new FileReader();      
   reader.readAsDataURL(this.fileData); 
   reader.onload = (_event) => { 
-    this.previewUrl = reader.result; 
+    this.url = reader.result; 
   }
 }
 
@@ -65,42 +71,44 @@ preview() {
     return this.registerForm.controls; }
 
     async onSubmit() {
-      console.log(this.registerForm.value)
       this.submitted = true;
       this.loading = true
       
       if (this.registerForm.invalid) {
-        console.log(this.registerForm.value)
         this.loading = false
         return;
       }else{
         this.submitted = true;
         try {
-          const urlI='user/updateprofile'
-          const formData = new FormBuilder;
+          const urlI='http://localhost:4065/user/updateprofile'
+          const formData = new FormData();
           console.log("++++++++++++++++++++++++++++++++++++++++")
           let firstName = this.registerForm.get('firstName').value
           let lastName = this.registerForm.get('lastName').value
           let image = this.fileData
+          console.log(this.fileData)
           let sessionData:any = JSON.parse(localStorage.getItem('currentUser'))
           let user_id = sessionData.userId
-          let dataObj = {
-            "first_name":firstName,
-            "last_name":lastName,
-            "user_id":user_id,
-            "image":image
-          }
-
-          console.log('formData',formData)
-        const res = await this.AuthService.postFormRequest(urlI,dataObj);
-        if(res.status == 1){
-          this.toastr.success(res.message)
+          
+          formData.append('profile_pic', this.fileData);
+          formData.append('first_name', firstName);
+          formData.append('last_name', lastName);
+          formData.append('user_id', user_id);
+          
+          const res:any =   this.http.post(urlI, formData)
+      .subscribe(result => {
+        console.log(res);
+        if(result['status'] == 1){
+          this.toastr.success(result['message'])
           this.loading = false;
           // this.router.navigate(["/"]);
         }else{
           this.loading = false;
-          this.toastr.warning(res.message)
+          this.toastr.warning(result['message'])
         }
+      })
+      console.log(res)
+      
         } catch (error) {
           console.log('err', error);
           this.loading = false;
@@ -111,14 +119,13 @@ preview() {
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]); // read file as data url
-
+      this.fileProgress(event)
       reader.onload = (event) => { // called once readAsDataURL is completed
         // console.log(event.target.result)
-        // this.url = event.target.result;
+        // if(event.target.result){
+        //   // this.url = event.target.result;
+        // }
       }
     }
-  }
-  public delete(){
-    this.url = '';
   }
 }
